8294bc599d0012d860a730e991888eefc31f83bb
Ezeh20 <buchimmaduabuchukwu@gmail.com>
2023-02-17 00:18:47 +0100

    Style: description styling

--- src/Pages/Destination/index.jsx
+++ src/Pages/Destination/index.jsx
@@ -7,7 +7,9 @@ import buttons from './destination-data'
 function Destination() {
   const urr = JSON.parse(localStorage.getItem('planets'))
   const [currentDestination, setCurrentDestionaton] = useState(urr ?? 0)
-  const planet = data.destinations[currentDestination].images.webp
+  const { images, name, description, distance, travel } =
+    data.destinations[currentDestination]
+  const { webp } = images
 
   useEffect(() => {
     localStorage.setItem('planets', JSON.stringify(currentDestination))
@@ -17,12 +19,12 @@ function Destination() {
     <div className={styles.destination}>
       <Layout>
         <div className={styles.destinationCover}>
-          <p className={`${styles.desTitle} heading4`}>
+          <p className={`${styles.desTitle} menu2`}>
             <span>01</span>Pick your destination
           </p>
           <div className={styles.desContent}>
             <div className={styles.ContentTop}>
-              <img src={planet} alt="planet" className={styles.planet} />
+              <img src={webp} alt="planet" className={styles.planet} />
             </div>
             <div className={styles.contentBottom}>
               <div className={styles.desBtnContainer}>
@@ -51,6 +53,23 @@ function Destination() {
                     )
                   })}
                 </div>
+                <div className={styles.planetData}>
+                  <p className={`${styles.desHeading} heading2`}>
+                    {name.toLocaleUpperCase()}
+                  </p>
+                  <p className={`${styles.desText} text`}>{description}</p>
+                </div>
+              </div>
+              <div className={styles.desLine} />
+              <div className={styles.desDuration}>
+                <div className={styles.distance}>
+                  <p className="menu2">AVG. DISTANCE</p>
+                  <p className="heading4">{distance.toLocaleUpperCase()}</p>
+                </div>
+                <div className={styles.time}>
+                  <p className="menu2">EST. TRAVEL TIME</p>
+                  <p className="heading4">{travel.toLocaleUpperCase()}</p>
+                </div>
               </div>
             </div>
           </div>
@@ -61,3 +80,59 @@ function Destination() {
 }
 
 export default Destination
+
+/**
+ * 
+ *   {data.destinations
+          .filter((_, idx) => idx === currentDestination)
+          .map((planets) => {
+            const { name, description, distance, images, travel } = planets
+            const { webp } = images
+            return (
+              <div key={name} className={styles.destinationCover}>
+                <p className={`${styles.desTitle} heading4`}>
+                  <span>01</span>Pick your destination
+                </p>
+                <div className={styles.desContent}>
+                  <div className={styles.ContentTop}>
+                    <img src={webp} alt="planet" className={styles.planet} />
+                  </div>
+                  <div className={styles.contentBottom}>
+                    {' '}
+                    <div className={styles.desBtnContainer}>
+                      <div className={styles.desBtn}>
+                        {buttons.map(({ id, des }) => {
+                          return (
+                            <button
+                              type="button"
+                              key={id}
+                              className={` ${
+                                currentDestination === id
+                                  ? `${`${styles.desButton} ${styles.btnActive}`}`
+                                  : `${styles.desButton}`
+                              }   menu2`}
+                              onClick={() => setCurrentDestionaton(id)}
+                            >
+                              <div>{des}</div>
+                              <div
+                                className={`${
+                                  currentDestination === id
+                                    ? `${`${styles.desLineActive}`}`
+                                    : `${styles.desLine}`
+                                }`}
+                              />
+                            </button>
+                          )
+                        })}
+                      </div>
+                      <div className={styles.planetInfo}>
+                        <p className="heading2">{name.toLocaleUpperCase()}</p>
+                        <p className="text">{description}</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )
+          })}
+ */
